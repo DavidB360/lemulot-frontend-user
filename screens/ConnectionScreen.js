662cf0a3f2e71439f11e37d51011055a5dd3f47e
Your name <damien.gassian@orange.fr>
2022-12-12 18:10:59 +0100

    save screen et App.tsx v1.1

---
+++ screens/ConnectionScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+export default function ConnectionScreen({ navigation }) {
+	return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
