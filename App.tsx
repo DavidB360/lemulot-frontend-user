348cdfbf80c305e259f4f18bd40f29f24d0b81b3
Your name <damien.gassian@orange.fr>
2022-12-13 11:45:29 +0100

    commit main avant pull

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ export default function App() {
 		<Provider store={store}>
 			<PersistGate persistor={persistor}>
 				<NavigationContainer>
-					<Stack.Navigator screenOptions={{ headerShown: true }}>
+					<Stack.Navigator screenOptions={{ headerShown: false }}>
 						<Stack.Screen name="Intro" component={IntroScreen} />
 						<Stack.Screen name="Home" component={HomeScreen} />
 						<Stack.Screen
--- screens/IntroScreen.js
+++ screens/IntroScreen.js
@@ -3,8 +3,29 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 export default function IntroScreen({ navigation }) {
 	return (
 		<View style={styles.container}>
-			<TouchableOpacity onPress={() => navigation.navigate("Home")}>
-				<Text>Go to HomeScreen</Text>
+			<View>
+				<Text style={styles.title}>Bienvenue sur le Mulot</Text>
+			</View>
+			<View style={styles.textcontent}>
+				<Text style={styles.text}>
+					Si tu en a marre de galérer, de tous le temps demander de
+					l’aide à tes enfants ou petit enfants. Si tu cherches une
+					assistance, un dépannage, ou simplement apprendre et
+					comprendre comment utiliser internet ou ton mobile. Alors
+					rassure toi tu es au bonne endroit !
+				</Text>
+				<Text style={styles.text}>
+					Tu trouveras ici des guides, des tutoriels et toutes l’aide
+					nécessaire pour résoudre tes difficultés et faire de toi un
+					vrai pro de l’informatique. Bientôt c’est à toi qu’on
+					demanderas de l’aide.
+				</Text>
+			</View>
+			<TouchableOpacity
+				style={styles.btn}
+				onPress={() => navigation.navigate("Home")}
+			>
+				<Text style={styles.textbtn}>Continuer</Text>
 			</TouchableOpacity>
 		</View>
 	);
@@ -13,7 +34,40 @@ export default function IntroScreen({ navigation }) {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		flexDirection: "column",
+		justifyContent: "space-around",
+		alignItems: "center",
+	},
+
+	title: {
+		fontSize: 25,
+	},
+
+	textcontent: {
+		padding: 10,
+	},
+
+	text: {
+		fontSize: 22,
+	},
+
+	btn: {
 		alignItems: "center",
 		justifyContent: "center",
+		backgroundColor: "#5db194",
+		borderBottomWidth: 8,
+		borderLeftWidth: 3,
+		borderRightWidth: 3,
+		borderRadius: 30,
+		borderStyle: "solid",
+		borderColor: "#a9a9a9",
+		width: 150,
+		height: 100,
+	},
+
+	textbtn: {
+		color: "#fff",
+		fontSize: 22,
+		// padding: 20,
 	},
 });
