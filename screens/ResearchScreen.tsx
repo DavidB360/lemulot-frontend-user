78e7ca32e4a31a3246aa987c5594f83ec08fd05d
DavidB360 <david.bonnet360@gmail.com>
2022-12-13 23:45:43 +0100

    ajout tableau exemple liste tuto pour travailler sur ResearchScreens

--- screens/ResearchScreen.tsx
+++ screens/ResearchScreen.tsx
@@ -1,6 +1,54 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function ResearchScreen({ navigation }: any) {
+
+// un tableau de tutos à charger pour mon Daminou :
+const tutorials = [
+    {
+        title: 'Créer une adresse email',
+        author: 'Professeur Mulot',
+        creationDate: '13-12-2022',
+        device: 'ordinateur',
+        category: 'logiciel',
+        difficulty: 'débutant',
+        content: '<View><Text>Nous allons créer une adresse gmail pas à pas avec toi ...</Text></View>'
+    },
+    {
+        title: 'Se connecter à un réseau wifi',
+        author: 'Mulot bricolo',
+        creationDate: '13-12-2022',
+        device: 'ordinateur',
+        category: 'connexion',
+        difficulty: 'débutant',
+        content: '<View><Text>Cliquer sur l\'icone à droite de votre barre des tâches ...</Text></View>'
+    },
+    {
+        title: 'Envoyer une photo dans WhatsApp',
+        author: 'Mulot influenceur',
+        creationDate: '13-12-2022',
+        device: 'smartphone',
+        category: 'communication',
+        difficulty: 'débutant',
+        content: '<View><Text>en bas de votre écran, appuyer sur le bouton en forme d\'appareil photo ...</Text></View>'
+    },
+    {
+        title: 'Nettoyer son ordinateur avec ccleaner',
+        author: 'Mulot hacker',
+        creationDate: '13-12-2022',
+        device: 'ordinateur',
+        category: 'logiciel',
+        difficulty: 'avancé',
+        content: '<View><Text>Télécharger l\'application ccleaner à l\'adresse suivante...</Text></View>'
+    },
+];
+
+
+//	  ()_|)
+//	   |oo|	   |			|\  /|
+//	   (_p)	  <	  Je dis	| \/ |
+//	__{|_)}	   |			|    |
+//	  _| |_
+
 	return (
 		<View style={styles.container}>
 			<TouchableOpacity onPress={() => navigation.navigate("Tuto")}>
