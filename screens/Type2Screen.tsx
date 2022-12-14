39a8ee15d511b91bbdac0d14322d6551a8d4b9b4
Your name <damien.gassian@orange.fr>
2022-12-14 11:35:03 +0100

    save screen Type1 à Type4 + css v1

--- screens/Type2Screen.tsx
+++ screens/Type2Screen.tsx
@@ -1,6 +1,6 @@
 import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 
-export default function TypeScreen({ navigation }: any) {
+export default function Type2Screen({ navigation }: any) {
 	return (
 		<View style={styles.container}>
 			<TouchableOpacity
@@ -11,37 +11,37 @@ export default function TypeScreen({ navigation }: any) {
 			</TouchableOpacity>
 			<TouchableOpacity
 				style={styles.btnRetour}
-				onPress={() => navigation.navigate("Home")}
+				onPress={() => navigation.navigate("Type1")}
 			>
 				<Text style={styles.textBtnRetour}>Retour</Text>
 			</TouchableOpacity>
 			<TouchableOpacity
-				style={styles.btnOrdinateur}
-				onPress={() => navigation.navigate("Type1")}
+				style={styles.btnImage}
+				onPress={() => navigation.navigate("Research")}
 			>
-				<Text style={styles.textBtnOrdinateur}>Ordinateur</Text>
+				<Text style={styles.textBtnImage}>Image</Text>
 				<Image
-					style={styles.imgBtnOrdinateur}
+					style={styles.imgBtnImage}
 					source={require("../assets/mulot_professeur.jpg")}
 				/>
 			</TouchableOpacity>
 			<TouchableOpacity
-				style={styles.btnMobile}
-				onPress={() => navigation.navigate("Type1")}
+				style={styles.btnSon}
+				onPress={() => navigation.navigate("Research")}
 			>
-				<Text style={styles.textBtnMobile}>Mobile</Text>
+				<Text style={styles.textBtnSon}>Son</Text>
 				<Image
-					style={styles.imgBtnMobile}
+					style={styles.imgBtnSon}
 					source={require("../assets/mulot_professeur.jpg")}
 				/>
 			</TouchableOpacity>
 			<TouchableOpacity
-				style={styles.btnTablette}
-				onPress={() => navigation.navigate("Type1")}
+				style={styles.btnVideo}
+				onPress={() => navigation.navigate("Research")}
 			>
-				<Text style={styles.textBtnTablette}>Tablette</Text>
+				<Text style={styles.textBtnVideo}>Video</Text>
 				<Image
-					style={styles.imgBtnTablette}
+					style={styles.imgBtnVideo}
 					source={require("../assets/mulot_professeur.jpg")}
 				/>
 			</TouchableOpacity>
@@ -107,7 +107,7 @@ const styles = StyleSheet.create({
 		textShadowRadius: 5,
 	},
 
-	btnOrdinateur: {
+	btnImage: {
 		marginBottom: 10,
 		marginTop: 80,
 		borderColor: "#a9a9a9",
@@ -129,7 +129,7 @@ const styles = StyleSheet.create({
 		elevation: 25,
 	},
 
-	textBtnOrdinateur: {
+	textBtnImage: {
 		fontSize: 22,
 		color: "#ffffff",
 		textShadowColor: "#000000",
@@ -137,12 +137,12 @@ const styles = StyleSheet.create({
 		textShadowRadius: 5,
 	},
 
-	imgBtnOrdinateur: {
+	imgBtnImage: {
 		width: 180,
 		height: 150,
 	},
 
-	btnMobile: {
+	btnSon: {
 		margin: 10,
 		borderColor: "#a9a9a9",
 		borderBottomWidth: 4,
@@ -163,7 +163,7 @@ const styles = StyleSheet.create({
 		elevation: 25,
 	},
 
-	textBtnMobile: {
+	textBtnSon: {
 		fontSize: 22,
 		color: "#ffffff",
 		textShadowColor: "#000000",
@@ -171,12 +171,12 @@ const styles = StyleSheet.create({
 		textShadowRadius: 5,
 	},
 
-	imgBtnMobile: {
+	imgBtnSon: {
 		width: 180,
 		height: 150,
 	},
 
-	btnTablette: {
+	btnVideo: {
 		margin: 10,
 		borderColor: "#a9a9a9",
 		borderBottomWidth: 4,
@@ -197,7 +197,7 @@ const styles = StyleSheet.create({
 		elevation: 25,
 	},
 
-	textBtnTablette: {
+	textBtnVideo: {
 		fontSize: 22,
 		color: "#ffffff",
 		textShadowColor: "#000000",
@@ -205,7 +205,7 @@ const styles = StyleSheet.create({
 		textShadowRadius: 5,
 	},
 
-	imgBtnTablette: {
+	imgBtnVideo: {
 		width: 180,
 		height: 150,
 	},
