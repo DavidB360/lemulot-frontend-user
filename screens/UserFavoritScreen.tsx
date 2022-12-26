2e2f6afe4e7ab296601452a35bfe414110ff086c
DavidB360 <david.bonnet360@gmail.com>
2022-12-26 19:09:57 +0100

    Dans la page UserFavoritScreen, chargement de Tabnavigator à la place de Favoris dans le reducer prevPage pour corriger erreur à l'appui sur le bouton retour depuis la page Tuto, inconvénient : on ne revient pas sur l'onglet favoris mais sur l'onglet des demandes d'aide.

--- screens/UserFavoritScreen.tsx
+++ screens/UserFavoritScreen.tsx
@@ -64,7 +64,7 @@ export default function UserFavoritScreen({
 						key={i}
 						onPress={() => {
 							dispatch(updateTuto(tutorial._id));
-							dispatch(updatePrevPage("Favoris"));
+							dispatch(updatePrevPage("TabNavigator"));
 							navigation.navigate("Tuto");
 						}}
 					>
